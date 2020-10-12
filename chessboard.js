7292df19e58145b87cfaf86901c7acba3c5336af
Marjoree <65003463+mfargas@users.noreply.github.com>
2020-10-12 13:37:29 -0700

    Update functions for each problem

--- chessboard.js
+++ chessboard.js
@@ -1,3 +1,11 @@
-//Write a program that creates a string that represents an 8×8 grid, using newline
-characters to separate lines. At each position of the grid there is either a space
-or a "#" character. The characters should form a chessboard.//
+// //Write a program that creates a string that represents an 8×8 grid, using newline
+// characters to separate lines. At each position of the grid there is either a space
+// or a "#" character. The characters should form a chessboard.//
+
+let size = 8;
+
+let grid;
+
+for( i=0; i<size; i++){
+    console.log(" "+ "#");
+}
