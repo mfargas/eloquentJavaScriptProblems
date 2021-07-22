f14c1aa3bda3fc72686b07f7921aef1027bfaacd
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-07-21 17:28:23 -0700

    Add problems on event handling

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # eloquentJavaScriptProblems
 Problem set to the chapters within Eloquent JavaScript by Marijn Haverbeke.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [What I learned](#what-i-learned)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
--- deepComparison.js
+++ deepComparison.js
@@ -7,4 +7,4 @@
 // To find out whether values should be compared directly(use the === operator for that) or have their properties compared, 
 // you can use the typeof operator.If it produces "object" for both values, you should do a deep comparison.But you have to take 
 // one silly exception into account: because of a historical accident, typeof null also produces "object".
-// The Object.keys function will be useful when you need to go over the prop - erties of objects to compare them.
+// The Object.keys function will be useful when you need to go over the properties of objects to compare them.
